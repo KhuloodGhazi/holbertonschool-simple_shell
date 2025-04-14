3d053199e4d26127783e4acea2be26f3da5bc997
ab10680 <10680@holbertonstudents.com>
2025-04-14 20:19:11 +0000

    Update main.c to match Betty style and handle non-interactive mode

--- main.c
+++ main.c
@@ -16,14 +16,22 @@ ssize_t prompt_and_read(char **line, size_t *len)
 {
 ssize_t nread;
 
+/* Only show prompt in interactive mode */
+if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+/* Read line from stdin */
 nread = getline(line, len, stdin);
 if (nread == -1)
 {
+/* Handle Ctrl+D (EOF): free memory and exit */
 free(*line);
+if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "\n", 1);
 exit(0);
 }
+
+/* Remove trailing newline */
 if ((*line)[nread - 1] == '\n')
 (*line)[nread - 1] = '\0';
 
@@ -39,28 +47,35 @@ void execute_command(char *line)
 pid_t pid;
 char *argv[2];
 
+/* Create child process */
 pid = fork();
 if (pid == 0)
 {
+/* In child: prepare arguments and execute command */
 argv[0] = line;
 argv[1] = NULL;
 
+/* Execute the command (must be full path) */
 if (execve(argv[0], argv, NULL) == -1)
 perror("./hsh");
+
+/* Exit child if execve fails */
 exit(0);
 }
 else if (pid > 0)
 {
+/* In parent: wait for child process */
 wait(NULL);
 }
 else
 {
+/* Handle fork error */
 perror("fork");
 }
 }
 
 /**
- * main - Entry point
+ * main - Entry point of the shell
  * Return: Always 0
  */
 int main(void)
@@ -68,9 +83,13 @@ int main(void)
 char *line = NULL;
 size_t len = 0;
 
+/* Shell loop: keeps running until user exits */
 while (1)
 {
+/* Show prompt and read input */
 prompt_and_read(&line, &len);
+
+/* Execute the input command */
 execute_command(line);
 }
 
