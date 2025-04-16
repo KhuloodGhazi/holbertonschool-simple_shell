28b3458c99b25bdeed668ab2f0ad411087f1859b
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-04-16 17:38:31 +0000

    Task 3: Handle command lines with arguments using strtok

--- shell.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-void execute_command(char *line, char **argv);
-char *trim_spaces(char *str);
-ssize_t prompt_and_read(char **line, size_t *len);
-
-#endif                     
